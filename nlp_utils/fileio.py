4fd014444c4fbfff8c37f03cae6e1aacb716b55f
Lee Aspitarte <aspitarte@gmail.com>
2021-01-26 11:10:03 -0800

    improve file loading

--- nlp_utils/fileio.py
+++ nlp_utils/fileio.py
@@ -1,12 +1,21 @@
 import sqlite3
+import os
 import pandas as pd
 
 def load_df(db_path):
     con = sqlite3.connect(db_path)
     cursor = con.cursor()
 
-    df = pd.read_sql_query("SELECT * FROM texts", con, index_col='ID')
-    df = df.dropna(subset=['processed_text'])
-    df = df[df['language'] == 'en']
+    df_meta = pd.read_sql_query("SELECT * FROM raw_text", con, index_col='ID')
+    df = pd.read_sql_query("SELECT * FROM processed_text", con, index_col='ID')
+    df = df[df['language'] == 'en'].dropna(subset=['processed_text'])
+    
+    df_out = pd.concat([df, df_meta.loc[df.index]], axis=1)
+    
 
-    return df 
+    return df_out 
+
+if __name__ == '__main__':
+    data_folder = r'C:\Users\aspit\Git\MHDLab-Projects\Energy Storage\data'
+
+    df = load_df(os.path.join(data_folder, 'nlp_justenergystorage_100.db'))
