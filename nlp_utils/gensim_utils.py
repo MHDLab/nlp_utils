1330281349202066a5bd1d735d4858f20591a28e
Bryce Yahn <byahn2@u.rochester.edu>
2021-07-06 13:14:33 -0400

    set doc_edge probability to 0 along the diagonal

--- nlp_utils/gensim_utils.py
+++ nlp_utils/gensim_utils.py
@@ -58,17 +58,40 @@ def gensim_topic_info(lda_model, data_words, id2word, lambda_=0.6):
 
     return df_topickeywords, doc_topic_probs
 
-def gensim_edge_info(lda_model, data_words, id2word, lambda_=0.6):
+def gensim_edge_info(lda_model, data_words, id2word, doc_topic_probs, lambda_=0.6):
     """
-    Extracts the top keywords for each set of two topics
+    Extracts the top keywords for each set of two topics, and the document-topic probability matrix from an LDA model
     """
     vocab, doc_l = get_vocab_docl(data_words, id2word)
 
-    topic_term = lda_model.get_topics()
-    topic_word_rel = topic_word_relevance(topic_term, doc_topic_probs, doc_l, lambda_=lambda_)
-    df_topickeywords = ldamodel_top_topic_words(topic_word_rel, vocab, top_n=10, val_fmt=r"{lbl}")
 
-    return df_topickeywords
+    topic_term = lda_model.get_topics() #the probability for each word in each topic, shape(num_topics, vocabulary_size) = (20, 55132)
+    
+    num_docs = doc_topic_probs.shape[0]
+    num_topics = topic_term.shape[0]
+    num_words = topic_term.shape[1]
+    
+    #create new matrixes for the 3D data
+    edge_term = np.ndarray((num_topics, num_topics, num_words))
+    doc_edge_probs = np.ndarray((num_docs, num_topics, num_topics))
+    row_labels = []
+
+    #fill the matrices and then reduced to 2D
+    for t1 in range(num_topics):
+        for t2 in range(num_topics):
+            edge_term[t1,t2,:] = topic_term[t1,:] * topic_term[t2,:]
+            row_labels.append('topic_'+str(t1+1)+ ', topic_'+str(t2+1))
+            if t1 == t2:
+                doc_edge_probs[:, t1, t2] = np.zeros((num_docs))
+            else:
+                doc_edge_probs[:, t1, t2] = doc_topic_probs[:,t1] * doc_topic_probs[:,t2]
+    edge_term = np.reshape(edge_term, (num_topics**2, num_words)) #(400, 55132)
+    doc_edge_probs = np.reshape(doc_edge_probs, (num_docs, num_topics**2))
+
+    edge_word_rel = topic_word_relevance(edge_term, doc_edge_probs, doc_l, lambda_=lambda_) #identify the most relevant word accounting for marginal probabilities, shape (num_topics, vocabulary_size)
+    df_edgekeywords = ldamodel_top_topic_words(edge_word_rel, vocab, top_n=10, row_labels=row_labels, val_fmt=r"{lbl}")
+
+    return df_edgekeywords, doc_edge_probs
 
 def bigram_stats(data_words, id2word):
     vocab, docl = get_vocab_docl(data_words, id2word)
