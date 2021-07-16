9bf2941086c4d0357aafe9f6ad2e55174cc89b28
Lee Aspitarte <aspitarte@gmail.com>
2021-07-15 22:46:14 -0700

    split out wordcount dict from top_words fn

--- nlp_utils/text_analysis.py
+++ nlp_utils/text_analysis.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-def top_words(corpus, num_words = 10, word_filter = None):
+
+def gen_wordcount_dict(corpus, word_filter):
     dic = defaultdict(int)
     for text in corpus:
         for word in text:
@@ -8,6 +9,10 @@ def top_words(corpus, num_words = 10, word_filter = None):
             else:
                 if word in word_filter:
                     dic[word] = dic[word] + 1    
+    return dic
+
+def top_words(corpus, num_words = 10, word_filter = None):
+    dic = gen_wordcount_dict(corpus, word_filter)
     top = sorted(dic.items(), key= lambda x:x[1], reverse=True)[0:num_words]
 
     return top 
