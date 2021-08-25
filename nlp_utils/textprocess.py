dc3a128bb643f23eaeafc38151212dca8009d96e
Lee Aspitarte <aspitarte@gmail.com>
2021-08-25 12:21:32 -0700

    add text processing pipeline

--- nlp_utils/textprocess.py
+++ nlp_utils/textprocess.py
@@ -25,6 +25,57 @@ def apply_fn_text(text, fn, **kwargs):
 
     return text_out
 
+import sys
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+sys.path.append(r'C:\Users\aspit\Git\MLEF-Energy-Storage\ES_TextData\mat2vec')
+from mat2vec.processing import MaterialsTextProcessor
+from . import text_analysis
+
+def text_processing_pipeline(texts, debug = False):
+    print('Removing stopwords')
+    en_stopwords  = stopwords.words('english')
+    all_stops = set(en_stopwords) | set(string.punctuation)
+
+    texts = texts.apply(lambda x: stopword_removal(x, all_stops))
+
+    if debug: print([w[0] for w in text_analysis.top_words(texts, num_words=30)])
+
+
+    print('Running Mat2Vec text processing')
+    # Basic code for implementing mat2vec. need to initialize the mat2vec repo git submodule in the ES_TextData folder. This replaces numbers with the string '<nUm>' which I have not implemented code to remove. 
+    materials_text_processor = MaterialsTextProcessor()
+
+    texts = texts.apply(
+        lambda x: materials_text_processor.process(x, exclude_punct=True, replace_elements = True)[0]
+    )
+
+    # mat2vec replaces numbers with '<nUm>', so remove this from the texts. 
+    texts = texts.apply(lambda x: [t for t in x if t != '<nUm>'])
+
+    if debug: print([w[0] for w in text_analysis.top_words(texts, num_words=30)])
+
+    print('Performing Lemmatization')
+
+    #Use lemmatizer to keep words readable and potentially improve modeling. At the cost of speed.
+    wn_lemmatizer = WordNetLemmatizer()
+
+    texts = texts.apply(lambda x: apply_fn_text(x, wn_lemmatizer.lemmatize))
+
+    if debug: print([w[0] for w in text_analysis.top_words(texts, num_words=30)])
+
+    print("Performing Porter Stemming")
+    ## Porter stemming is more extreme than lemmatization and really makes different forms of words the same, but those root forms can be hard to read. 
+    porter_stemmer = PorterStemmer()
+
+    texts = texts.apply(lambda x: apply_fn_text(x, porter_stemmer.stem))
+
+    if debug: print([w[0] for w in text_analysis.top_words(texts, num_words=30)])
+
+    return texts
+
+
 
 """
 
