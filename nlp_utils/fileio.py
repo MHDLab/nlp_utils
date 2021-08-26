4c68ac6ac1ee16434ceb80f66dc7b0b7e0978263
Lee Aspitarte <aspitarte@gmail.com>
2021-08-25 23:04:15 -0700

    add load_df_semantic

--- nlp_utils/fileio.py
+++ nlp_utils/fileio.py
@@ -2,7 +2,53 @@ import sqlite3
 import os
 import pandas as pd
 
-def load_df(db_path):
+
+
+def load_df_semantic(con, ids, dataset='soc', cust_idx_name=None):
+    """
+    load a series of papers from either semantic open corpus(soc) or s2orc datasets.
+    cust_idx_name: overrides default index to search through (e.g. doi)
+
+    TODO: Perhaps names should be made consistent here. 
+    """
+    if dataset == 'soc':
+        idx = 'id'
+    elif dataset == 's2orc':
+        idx = 'paper_id'
+    else:
+        raise ValueError("dataset must be 'soc' or 's2orc'")
+
+    #search through database using a different index like doi
+    if cust_idx_name != None:
+        search_idx = cust_idx_name
+    else:
+        search_idx = idx
+
+
+    table_info = con.execute(f'PRAGMA table_info(raw_text);').fetchall()
+    columns = [t[1] for t in table_info]
+
+    cursor = con.cursor()
+    cursor.execute(
+        "SELECT * from raw_text where {} in (\"{}\")".format(search_idx, "\", \"".join(ids))
+        )
+    results = cursor.fetchall()
+
+    df = pd.DataFrame(results, columns=columns).set_index(idx)
+    df['year'] = df['year'].astype(float).astype(int)
+
+    df['years_ago'] = abs(2022 - df['year'])
+
+    if dataset == 'soc':
+        df['inCitations'] = df['inCitations'].apply(lambda x: x.strip('[]').replace("'", "").replace(" ", "").split(','))
+        df['outCitations'] = df['outCitations'].apply(lambda x: x.strip('[]').replace("'", "").replace(" ", "").split(','))
+    elif dataset == 's2orc':
+        df['inbound_citations'] = df['inbound_citations'].apply(lambda x: x.strip('[]').split(','))
+        df['outbound_citations'] = df['outbound_citations'].apply(lambda x: x.strip('[]').split(','))
+
+    return df
+
+def load_df_MA(db_path):
     con = sqlite3.connect(db_path)
     cursor = con.cursor()
 
@@ -15,7 +61,6 @@ def load_df(db_path):
     df_out = pd.concat([df, df_meta.loc[df.index]], axis=1)
 
     df_out = df_out.dropna(subset=['processed_text'])
-    
 
     return df_out 
 
