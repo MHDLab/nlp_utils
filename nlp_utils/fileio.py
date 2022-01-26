f621e5f69aca0cd83d662c21c99169d1587d4dc9
aspitarl <aspitarte@gmail.com>
2022-01-25 19:06:57 -0800

    delete microsoft academic load function
    obsolete

--- nlp_utils/fileio.py
+++ nlp_utils/fileio.py
@@ -105,21 +105,6 @@ def get_columns_as_df(con, columns, search_limit=None, dataset='soc', table_name
 
     return df
 
-def load_df_MA(db_path):
-    con = sqlite3.connect(db_path)
-    cursor = con.cursor()
-
-    df_meta = pd.read_sql_query("SELECT * FROM raw_text", con, index_col='ID')
-    df = pd.read_sql_query("SELECT * FROM processed_text", con, index_col='ID')
-
-    if 'language' in df.columns:
-        df = df[df['language'] == 'en'].dropna(subset=['processed_text'])
-    
-    df_out = pd.concat([df, df_meta.loc[df.index]], axis=1)
-
-    df_out = df_out.dropna(subset=['processed_text'])
-
-    return df_out 
 
 def load_df_SEAMs(db_folder):
     """
